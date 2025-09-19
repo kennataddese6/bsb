e0634785de32da5097b0b8735ffbda6557aefc66
kennataddese6 <kennataddese6@gmail.com>
2025-09-19 11:10:34 +0300

    refacor: remove comemnts

--- src/app/[lang]/(dashboard)/(private)/dashboards/crm/SalesChartHeaderServer.tsx
+++ src/app/[lang]/(dashboard)/(private)/dashboards/crm/SalesChartHeaderServer.tsx
@@ -2,22 +2,16 @@
 
 import Grid from '@mui/material/Grid2'
 
-import { /* getSalesData, */ getSalesPerson } from '@/app/server/actions'
+import { getSalesPerson } from '@/app/server/actions'
 import CRMBarChartHeader from '@/views/dashboards/crm/CRMBarchartHeader'
 
-const SalesChartHeaderServer = async (/* {
-  searchParams
-}: {
-  searchParams: { freq?: 'yearly' | 'quarterly'; sales?: string }
-} */) => {
-  // const resolvedSearchParams = await searchParams
-
+const SalesChartHeaderServer = async () => {
   const salesPersons = await getSalesPerson({ page: '1', size: '10' })
 
   return (
     <Grid container spacing={6}>
       <Grid size={{ xs: 12 }}>
-        <CRMBarChartHeader /* data={data} */ salesPersons={salesPersons.data} />
+        <CRMBarChartHeader salesPersons={salesPersons.data} />
       </Grid>
     </Grid>
   )

5e25a28284dd1c3cfc79db7bab0f9268ce4a075b
kennataddese6 <kennataddese6@gmail.com>
2025-09-19 11:20:32 +0300

    refactor

--- src/app/[lang]/(dashboard)/(private)/dashboards/crm/page.tsx
+++ src/app/[lang]/(dashboard)/(private)/dashboards/crm/page.tsx
@@ -1,15 +1,13 @@
-// Components Imports
+// ** MUI Imports
 import { Suspense, use } from 'react'
 
+// ** MUI Imports
 import Card from '@mui/material/Card'
 
+// ** Components Imports
 import ChartSkeleton from '@/views/dashboards/crm/components/ChartSkeleton'
 import SalesChartServer from './SalesChartServer'
 import SalesChartHeaderServer from './SalesChartHeaderServer'
-
-// import EarningReports from '@views/dashboards/crm/EarningReports'
-// import SalesAnalytics from '@views/dashboards/crm/SalesAnalytics'
-// import SalesStats from '@views/dashboards/crm/SalesStats'
 
 const DashboardCRM = ({ searchParams }: { searchParams: Promise<{ freq?: 'yearly' | 'quarterly' }> }) => {
   const sp = use(searchParams) as { freq?: 'yearly' | 'quarterly'; sales?: string }
@@ -19,8 +17,8 @@ const DashboardCRM = ({ searchParams }: { searchParams: Promise<{ freq?: 'yearly
 
   return (
     <Card className='bs-full' sx={{ width: '100%' }}>
-      <Suspense key={suspenseKey} /* fallback={<h1>Loading...</h1>} */>
-        <SalesChartHeaderServer /* searchParams={resolvedSearchParams} */ />
+      <Suspense key={suspenseKey}>
+        <SalesChartHeaderServer />
       </Suspense>
       <Suspense key={suspenseKey} fallback={<ChartSkeleton />}>
         <SalesChartServer searchParams={resolvedSearchParams} />

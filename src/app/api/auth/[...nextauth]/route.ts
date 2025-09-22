e27d209391b8c1ade53528f619304c6232a7dd06
kennataddese6 <kennataddese6@gmail.com>
2025-09-22 11:32:48 +0300

    clean code

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,11 +4,6 @@ import NextAuth from 'next-auth'
 // Lib Imports
 import { authOptions } from '@/libs/auth'
 
-/*
- * As we do not have backend server, the refresh token feature has not been incorporated into the template.
- * Please refer https://next-auth.js.org/tutorials/refresh-token-rotation link for a reference.
- */
-
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
